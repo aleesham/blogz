3866316ef7b6e4ecdaf20e27b5e0cfc407501f09
avmoran92 <avmoran@wustl.edu>
2017-06-15 16:25:18 -0500

    Works, but code could probably use some work.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
@@ -14,38 +15,37 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String())
+    body = db.Column(db.String(1000))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-def get_all_entries():
-    return Blog.query.all()
 
-
-## I need to edit new-post to effectively add blog posts. 
-post1 = Blog("Post 1", "This is my post.")
-post1.id = 1
-post2 = Blog("Post 2", "This is my post.")
-post2.id = 2
-posts = [post1, post2]
-
-
-## This needs to be edited
-@app.route('/blog')
+### Everything works. Now go back and actually read the directions to see if everything works. Also I feel like this route can be done better, but I'm not really sure how just yet. 
+@app.route('/blog', methods = ['GET', 'POST'])
 def index():
     blog_id = request.args.get('id')
+
+    if request.method == 'POST':
+        new_post_title = request.form['title']
+        new_post_body = request.form['body']
+        post = Blog(new_post_title, new_post_body)
+        db.session.add(post)
+        db.session.commit()
+
     if blog_id:
-        ## Change this to render the actual blog.
-        return render_template('display-post.html', title = 'This works.', post=post)
-    return render_template('blog.html', title = post.title, posts=posts)
+        post = Blog.query.filter_by(id=blog_id).first()
+        return render_template('display-post.html', post = post)
+
+    return render_template('blog.html', title = "Build-a-Blog", posts=Blog.query.all())
 
     
-@app.route('/newpost', methods = ['GET','POST'])
-def post():
-    
-    return render_template('add-blog.html', title = 'Build-a-Blog')
+@app.route('/newpost', methods = ['GET', 'POST'])
+def display():
+    new_action = "/blog?id=" + str(len(Blog.query.all()) + 1)
+    return render_template('add-blog.html', new_action = new_action, title = 'Build-a-Blog')
+
 
 if __name__ == '__main__':
     app.run()
