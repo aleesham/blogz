9f2353d1ad0e3fea4cb96f6dd4da5ab109f25d32
avmoran92 <avmoran@wustl.edu>
2017-06-15 14:23:11 -0500

    Finished with all by ORM

--- main.py
+++ main.py
@@ -23,16 +23,28 @@ class Blog(db.Model):
 def get_all_entries():
     return Blog.query.all()
 
-posts = []
 
-## Edit all of this. 
+## I need to edit new-post to effectively add blog posts. 
+post1 = Blog("Post 1", "This is my post.")
+post1.id = 1
+post2 = Blog("Post 2", "This is my post.")
+post2.id = 2
+posts = [post1, post2]
+
+
+## This needs to be edited
 @app.route('/blog')
 def index():
-    return render_template('blog.html', title = 'Build-a-Blog', posts=posts)
+    blog_id = request.args.get('id')
+    if blog_id:
+        ## Change this to render the actual blog.
+        return render_template('display-post.html', title = 'This works.', post=post)
+    return render_template('blog.html', title = post.title, posts=posts)
 
     
 @app.route('/newpost', methods = ['GET','POST'])
 def post():
+    
     return render_template('add-blog.html', title = 'Build-a-Blog')
 
 if __name__ == '__main__':
