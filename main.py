fcd7faae9f88fe366c64e31717a6384c8bb177e4
avmoran <avmoran@wustl.edu>
2018-01-17 10:23:37 -0600

    Implemented MVC

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-from flask import Flask, redirect, render_template, request, session, flash, get_flashed_messages
-from flask_sqlalchemy import SQLAlchemy
-from hashutils import make_pw_hash, check_pw_hash
+from flask import redirect, render_template, request, session, flash, get_flashed_messages
+from hashutils import check_pw_hash
+from app import app, db
+from models import User, Blog
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-app.secret_key = 'y337kGcys&zP3B'
-db = SQLAlchemy(app)
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(3000))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    ### TODO add a DateTime column eventually so we can sort with respect to it.
-
-    def __init__(self, title, body, owner):
-        self.title = title
-        self.body = body
-        self.owner = owner
-
-    def __repr__(self):
-        return '<Blog {0}>'.format(self.title)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120))
-    pw_hash = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref = 'owner')
-
-    def __init__(self, username, password):
-        self.username = username
-        self.pw_hash = make_pw_hash(password)
 
 @app.before_request
 def require_login():
@@ -135,6 +105,8 @@ def post():
             return redirect('/blog?id='+str(blog.id))
 
     return render_template('newpost.html', title="Add a Blog Entry")
+
+app.secret_key = 'y337kGcys&zP3B'
 
 if __name__ == '__main__':
     app.run()
